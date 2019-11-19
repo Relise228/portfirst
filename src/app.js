a17c608025b0a473f64ff5fc9186b222279c3556
Relise228 <andreysmit43@gmail.com>
2019-11-19 10:14:44 +0200

    Frist commit

---
+++ src/app.js
@@ -0,0 +1,7 @@
+import "./js/rainbows.js";
+import "./js/unicorns.js";
+import "./scss/fonts.scss";
+import "./scss/base.scss";
+import "./scss/header.scss";
+import "./scss/main.scss";
+import "./scss/footer.scss";
